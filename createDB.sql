6ca60e16eb042513b7070eed78b3f0a99d707cd5
kamel <kamelfakihh@gmail.com>
2021-02-14 12:37:23 +0200

    made changes to database schema and added favourotes route

--- createDB.sql
+++ createDB.sql
@@ -6,21 +6,22 @@ ALTER ROLE admin CREATEDB;
 
 CREATE DATABASE bookshare;
 
+\c bookshare
+
 create extension "uuid-ossp";
 
 CREATE TABLE "Users" (
-    "ID" uuid NOT NULL DEFAULT uuid_generate_v4(),
+    "ID" uuid   NOT NULL DEFAULT uuid_generate_v4(),
     "FirstName" varchar(255)   NOT NULL,
     "LastName" varchar(255)   NOT NULL,
     "Email" varchar(255)   NOT NULL,
     "UserName" varchar(255)   NOT NULL,
-    "Contact" varchar(20),
-    "Password" varchar(255)   NOT NULL,
-    "Blocked" boolean,
-    "LastActive" timestamp,
-    "CreatedAt" timestamp NOT NULL DEFAULT NOW(),
-    "UpdatedAt" timestamp   NOT NULL DEFAULT NOW(),
+    "PhoneNumber" varchar(20),
+    "Password" varchar(128)   NOT NULL,
+    "ActiveAt" timestamp,
+    "CreatedAt" timestamp   NOT NULL DEFAULT NOW(),
     "DeletedAt" timestamp,
+    "BlockedAt" timestamp,
     CONSTRAINT "pk_Users" PRIMARY KEY (
         "ID"
      ),
@@ -30,21 +31,20 @@ CREATE TABLE "Users" (
 );
 
 CREATE TABLE "Books" (
-    "ID" serial NOT NULL,
-    "UserId" uuid NOT NULL,
+    "ID" int GENERATED ALWAYS AS IDENTITY,
+    "UserId" uuid   NOT NULL,
     "Title" varchar(255)   NOT NULL,
     "Author" varchar(255),
     "PublishDate" date,
     "Description" varchar(1000),
-    "Image_url" : varchar(2083),
-    "Genre" int  NOT NULL,
-    "Level" int   NOT NULL,
+    "Image_url" varchar(2083),
+    "Genre" int NOT NULL,
     "ISBN" varchar(14),
-    "Available" boolean DEFAULT TRUE,
-    "Price" int NOT NULL DEFAULT 0,
-    "Location" varchar(255)   NOT NULL,
+    "IsAvailable" boolean NOT NULL DEFAULT TRUE,
+    "Price" float   NOT NULL DEFAULT 0,
+    "PriceCurrency" varchar(3)   NOT NULL DEFAULT 'LBP',
+    "Location" varchar(255),
     "CreatedAt" timestamp   NOT NULL DEFAULT NOW(),
-    "UpdatedAt" timestamp   NOT NULL DEFAULT NOW(),
     "DeletedAt" timestamp,
     CONSTRAINT "pk_Books" PRIMARY KEY (
         "ID"
@@ -52,12 +52,11 @@ CREATE TABLE "Books" (
 );
 
 CREATE TABLE "Notification" (
-    "ID" serial  NOT NULL,
-    "UserId" uuid  NOT NULL,
-    "Message" varchar(300) NOT NULL,
-    "Viewed" boolean NOT NULL DEFAULT FALSE,
+    "ID" int GENERATED ALWAYS AS IDENTITY,
+    "UserId" uuid   NOT NULL,
+    "Message" varchar(300)   NOT NULL,
     "DeletedAt" timestamp,
-    "AddedAt" timestamp   NOT NULL DEFAULT NOW(),
+    "AddedAt" timestamp NOT NULL DEFAULT NOW(),
     "ViewedAt" timestamp,
     CONSTRAINT "pk_Notification" PRIMARY KEY (
         "ID"
@@ -65,7 +64,7 @@ CREATE TABLE "Notification" (
 );
 
 CREATE TABLE "Genres" (
-    "ID" serial   NOT NULL,
+    "ID" int GENERATED ALWAYS AS IDENTITY,
     "Name" varchar(255)   NOT NULL,
     "Description" varchar(1000),
     CONSTRAINT "pk_Genres" PRIMARY KEY (
@@ -73,26 +72,19 @@ CREATE TABLE "Genres" (
      )
 );
 
-CREATE TABLE "Levels" (
-    "ID" serial   NOT NULL,
-    "Name" varchar(255)   NOT NULL,
-    "Description" varchar(1000),
-    CONSTRAINT "pk_Levels" PRIMARY KEY (
+CREATE TABLE "Saved" (
+    "ID" int GENERATED ALWAYS AS IDENTITY,
+    "UserId" uuid   NOT NULL,
+    "BookId" int   NOT NULL,
+    "AddedAt" timestamp   NOT NULL DEFAULT NOW(),
+    "RemovedAt" timestamp   NOT NULL,
+    CONSTRAINT "pk_Saved" PRIMARY KEY (
         "ID"
      )
 );
 
-CREATE TABLE "Favourites" (
-    "UserId" uuid   NOT NULL,
-    "BookId" int   NOT NULL,
-    "AddedAt" timestamp   NOT NULL DEFAULT NOW(),
-    CONSTRAINT "pk_Favourites" PRIMARY KEY (
-        "UserId","BookId"
-     )
-);
-
 CREATE TABLE "Viewed" (
-    "ID" serial NOT NULL,
+    "ID" int GENERATED ALWAYS AS IDENTITY,
     "UserId" uuid   NOT NULL,
     "BookId" int   NOT NULL,
     "ViewedAt" timestamp   NOT NULL DEFAULT NOW(),
@@ -101,33 +93,32 @@ CREATE TABLE "Viewed" (
      )
 );
 
-CREATE TABLE "Given_Away" (
-    "ownerId" uuid   NOT NULL,
+CREATE TABLE "GivenAway" (
+    "OwnerId" uuid   NOT NULL,
     "BookId" int   NOT NULL,
-    "IssuedAt" timestamp   NOT NULL DEFAULT NOW(),
-    CONSTRAINT "pk_Given_away" PRIMARY KEY (
-        "ownerId","BookId"
+    "GivenAt" timestamp   NOT NULL DEFAULT NOW(),
+    CONSTRAINT "pk_GivenAway" PRIMARY KEY (
+        "OwnerId","BookId"
      )
 );
 
 CREATE TABLE "Reports" (
     "ID" uuid   NOT NULL,
-    "IssuedFrom" uuid   NOT NULL,
-    "BookId" int   NOT NULL,
+    "IssuedBy" uuid   NOT NULL,
+    "BookID" int   NOT NULL,
     "Type" int   NOT NULL,
-    "Description" varchar(500),
-    "Reviewed" boolean  NOT NULL DEFAULT FALSE,
-    "CreatedAt" timestamp   NOT NULL DEFAULT NOW(),
-    "ReviewedAt" timestamp,
+    "Description" varchar(500)   NOT NULL,
+    "IssuedAt" timestamp   NOT NULL DEFAULT NOW(),
+    "ReviewedAt" timestamp   NOT NULL,
     CONSTRAINT "pk_Reports" PRIMARY KEY (
         "ID"
      )
 );
 
 CREATE TABLE "ReportTypes" (
-    "ID" serial  NOT NULL,
+    "ID" int GENERATED ALWAYS AS IDENTITY,
     "Name" varchar(255)   NOT NULL,
-    "Description" varchar(500),
+    "Description" varchar(500)   NOT NULL,
     CONSTRAINT "pk_ReportTypes" PRIMARY KEY (
         "ID"
      )
@@ -150,16 +141,13 @@ REFERENCES "Users" ("ID");
 ALTER TABLE "Books" ADD CONSTRAINT "fk_Books_Genre" FOREIGN KEY("Genre")
 REFERENCES "Genres" ("ID");
 
-ALTER TABLE "Books" ADD CONSTRAINT "fk_Books_Level" FOREIGN KEY("Level")
-REFERENCES "Levels" ("ID");
-
 ALTER TABLE "Notification" ADD CONSTRAINT "fk_Notification_UserId" FOREIGN KEY("UserId")
 REFERENCES "Users" ("ID");
 
-ALTER TABLE "Favourites" ADD CONSTRAINT "fk_Favourites_UserId" FOREIGN KEY("UserId")
+ALTER TABLE "Saved" ADD CONSTRAINT "fk_Saved_UserId" FOREIGN KEY("UserId")
 REFERENCES "Users" ("ID");
 
-ALTER TABLE "Favourites" ADD CONSTRAINT "fk_Favourites_BookId" FOREIGN KEY("BookId")
+ALTER TABLE "Saved" ADD CONSTRAINT "fk_Saved_BookId" FOREIGN KEY("BookId")
 REFERENCES "Books" ("ID");
 
 ALTER TABLE "Viewed" ADD CONSTRAINT "fk_Viewed_UserId" FOREIGN KEY("UserId")
@@ -168,20 +156,38 @@ REFERENCES "Users" ("ID");
 ALTER TABLE "Viewed" ADD CONSTRAINT "fk_Viewed_BookId" FOREIGN KEY("BookId")
 REFERENCES "Books" ("ID");
 
-ALTER TABLE "Given_Away" ADD CONSTRAINT "fk_Given_away_ownerId" FOREIGN KEY("ownerId")
+ALTER TABLE "GivenAway" ADD CONSTRAINT "fk_GivenAway_OwnerId" FOREIGN KEY("OwnerId")
 REFERENCES "Users" ("ID");
 
-ALTER TABLE "Given_Away" ADD CONSTRAINT "fk_Given_away_BookId" FOREIGN KEY("BookId")
+ALTER TABLE "GivenAway" ADD CONSTRAINT "fk_GivenAway_BookId" FOREIGN KEY("BookId")
 REFERENCES "Books" ("ID");
 
-ALTER TABLE "Given_Away" ADD CONSTRAINT "fk_Given_away_IssuedTo" FOREIGN KEY("IssuedTo")
+ALTER TABLE "Reports" ADD CONSTRAINT "fk_Reports_IssuedBy" FOREIGN KEY("IssuedBy")
 REFERENCES "Users" ("ID");
 
-ALTER TABLE "Reports" ADD CONSTRAINT "fk_Reports_IssuedFrom" FOREIGN KEY("IssuedFrom")
-REFERENCES "Users" ("ID");
-
-ALTER TABLE "Reports" ADD CONSTRAINT "fk_Reports_BookId" FOREIGN KEY("BookId")
+ALTER TABLE "Reports" ADD CONSTRAINT "fk_Reports_BookID" FOREIGN KEY("BookID")
 REFERENCES "Books" ("ID");
 
 ALTER TABLE "Reports" ADD CONSTRAINT "fk_Reports_Type" FOREIGN KEY("Type")
 REFERENCES "ReportTypes" ("ID");
+
+CREATE VIEW "AvailableBooks" as
+	SELECT B."ID", B."Title", B."Author", B."PublishDate", B."Description", B."Image_url",
+           B."ISBN", B."Price", B."PriceCurrency", B."Location", B."CreatedAt" , 
+           G."Name" as "Genre", U."UserName"
+	FROM ("Books" B INNER JOIN "Genres" G ON B."Genre" = G."ID") INNER JOIN "Users" U ON U."ID" = B."UserId"
+	WHERE B."IsAvailable" = True AND B."DeletedAt" IS NULL AND U."DeletedAt" IS NULL;
+
+CREATE VIEW "DeletedBooks" as 
+    SELECT *
+    FROM "Books"
+    WHERE "Books"."DeletedAt" IS NOT NULL;
+
+CREATE VIEW "NotDeletedBooks" as
+    SELECT *
+    FROM "Books"
+    WHERE "Books"."DeletedAt" IS NULL;
+
+CREATE VIEW "GetBooks" as
+	SELECT "AvailableBooks"."ID", "AvailableBooks"."Title", "AvailableBooks"."Author"
+    FROM "AvailableBooks" ;
--- routes/books.js
+++ routes/books.js
@@ -66,76 +66,99 @@ router.get('/', async (req, res) => {
 // @access      Public
 router.get('/:id', async (req, res) => {
 
-    // get book id to fetch its details
-    const BookId = req.params.id;
-    const {userId} = req.session;
+    try {
+        
+        // get book id from params
+        const BookId = req.params.id;
+        const {userId} = req.session;
 
-    const result = await pool.query(
-        `select "Books"."ID", "Books"."Title", "Books"."Author", "Books"."Description", 
-        "Books"."PublishDate", "Books"."Image_url", "Books"."ISBN", "Books"."Available", 
-        "Genres"."Name" AS "Genre", "Levels"."Name" AS "Level", "Books"."Location", "Books"."CreatedAt" 
-        from "Books", "Genres", "Levels" 
-        WHERE "Books"."Level" = "Levels"."ID" and "Books"."Genre" = "Genres"."ID" and "Books"."ID" = ${BookId} and "Books"."DeletedAt" is null`
-    );
+        // fetch book from db
+        const result = await pool.query(
+            `select "Books"."ID", "Books"."Title", "Books"."Author", "Books"."Description", 
+            "Books"."PublishDate", "Books"."Image_url", "Books"."ISBN", "Books"."Available", 
+            "Genres"."Name" AS "Genre", "Levels"."Name" AS "Level", "Books"."Location", "Books"."CreatedAt" 
+            from "Books", "Genres", "Levels" 
+            WHERE "Books"."Level" = "Levels"."ID" and "Books"."Genre" = "Genres"."ID" and "Books"."ID" = ${BookId} and "Books"."DeletedAt" is null`
+        );
 
-     // return result to client
-    if(result.rowCount != 0){
-        // send book to user
-        res.status(200).json({
-            body : result.rows,
-            message : null
-        });
+        // check if book is in user's favourites list
+        let exists = false;
+        if(userId){
+            const fav = await pool.query(`SELECT EXISTS (SELECT * FROM "Favourites" 
+                                         WHERE "UserId" = $1 AND "BookId" = $2);`, [userId, BookId]); 
+            exists = fav.rows[0].exists;
+        }
 
-        // book to user's viewed history
-        if(userId) pool.query('INSERT INTO "Viewed" ("UserId", "BookId") VALUES ($1, $2)', [userId, BookId]);
+        // return result
+        if(result.rowCount != 0){
+            
+            res.status(200).json({
+                body : {
+                    ...result.rows[0],
+                    favourite : exists                },
+                message : null
+            });
 
-        return;
+            // add book to user's viewed history
+            if(userId) pool.query('INSERT INTO "Viewed" ("UserId", "BookId") VALUES ($1, $2)', [userId, BookId]);
 
-    }else{
-        return res.status(200).json({
-            message : "0 books were found"
-        });
+            return;
+
+        }else{
+            return res.status(200).json({
+                message : "0 books were found"
+            });
+        }
+
+    } catch(error){
+        console.log(error);
+        res.status(500).json({message : 'internal error, please try again !'});
     }
-
 });
 
 // @route       POST api/books/post
 // @desc        post a new book
 // @access      Protected
 router.post('/post', protected, async (req, res) => {
-
-    // get book data from request
-    const {
-        Title, Author, Description,
-        PublishDate, Genre, Level,
-        ISBN, Location, image_url
-    } = req.body;
-
-    const {userId} = req.session;
-
-    // check for null values in request (columns that are NOT NULL in the Books table)
-    if( !Title || !Genre || !Level || !Location){
-        return res.status(400).json({message : "invalid post parameters !"});
-    }
-
-    const query = `INSERT INTO "Books" ("UserId", "Title", "Author", "Description", "PublishDate", "Genre", "Level", "ISBN", "Location", "Image_url") 
-                   VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) `;
-
-    // add book to db
-    const result = await pool.query(
-        query, 
-        [
-            userId, Title, Author || null, Description || null, PublishDate || null,
-            Genre, Level, ISBN || null, Location, image_url || null
-        ]
-    );
     
-    if(result.rowCount >= 1){
-        return res.status(200).json({message : "added 1 book"});
-    }else{
-        return res.status(400).json({message : "failed to add book"});
-    }
+    try {
 
+        // get book data from request
+        const {
+            Title, Author, Description,
+            PublishDate, Genre, Level,
+            ISBN, Location, image_url
+        } = req.body;
+
+        const {userId} = req.session;
+
+        // check for null values in request (columns that are NOT NULL in the Books table)
+        if( !Title || !Genre || !Level || !Location){
+            return res.status(400).json({message : "invalid post parameters !"});
+        }
+
+        const query = `INSERT INTO "Books" ("UserId", "Title", "Author", "Description", "PublishDate", "Genre", "Level", "ISBN", "Location", "Image_url") 
+                    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) `;
+
+        // add book to db
+        const result = await pool.query(
+            query, 
+            [
+                userId, Title, Author || null, Description || null, PublishDate || null,
+                Genre, Level, ISBN || null, Location, image_url || null
+            ]
+        );
+        
+        if(result.rowCount >= 1){
+            return res.status(200).json({message : "added 1 book"});
+        }else{
+            return res.status(400).json({message : "failed to add book"});
+        }
+
+    } catch(error){
+        console.log(error);
+        res.status(500).json({message : 'internal error, please try again !'});
+    }
 });
 
 // @route       DELETE api/books/:id
@@ -143,14 +166,63 @@ router.post('/post', protected, async (req, res) => {
 // @access      Protected
 router.delete('/:id', protected, async (req, res) => {
 
-    const BookId = req.params.id;
+    try {
 
-    const result = await pool.query('UPDATE "Books" SET "DeletedAt" = NOW() WHERE "Books"."ID" = $1', [BookId]); 
+        const BookId = req.params.id;
 
-    if(result.rowCount >= 1){
-        return res.status(200).json({message : "deleted 1 book"});
-    }else{
-        return res.status(400).json({message : "failed to delete book"});
+        const result = await pool.query('UPDATE "Books" SET "DeletedAt" = NOW() WHERE "Books"."ID" = $1', [BookId]); 
+
+        if(result.rowCount >= 1){
+            return res.status(200).json({message : "deleted 1 book"});
+        }else{
+            return res.status(400).json({message : "failed to delete book"});
+        }
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({message : 'internal error, please try again !'});
+    }
+});
+
+// @route       POST api/books/fav/:id
+// @desc        adds/removes a book from favourites list
+// @access      Protected
+router.post('/fav/:id', protected, async (req, res) => {
+
+    try {
+
+        const {userId} = req.session;
+        const BookId = req.params.id;
+
+        // check if book is already in favourites list
+        let result = await pool.query(`SELECT EXISTS (SELECT * FROM "Favourites" 
+                                         WHERE "UserId" = $1 AND "BookId" = $2);`, [userId, BookId]);
+
+        const { exists } = result.rows[0];
+
+        if(!exists){
+            // if not, add it to favourites
+            result = await pool.query(`INSERT INTO "Favourites" ("UserId", "BookId") VALUES ($1, $2);`, [userId, BookId]);
+
+            if(result.rowCount >= 1){
+                return res.status(200).json({message : "added book to favourites"});
+            }else{
+                return res.status(503).json({message : "failed to add book to favourites"});
+            }
+        }else{
+            // else, remove it from favourites
+            result = await pool.query(`DELETE FROM "Favourites" WHERE "UserId" = $1 AND "BookId" = $2`, [userId, BookId]);
+
+            if(result.rowCount >= 1){
+                return res.status(200).json({message : "removed book from favourites"});
+            }else{
+                return res.status(503).json({message : "failed to remove book from favourites"});
+            }
+        }
+
+    } catch(error){
+        console.log(error);
+        res.status(500).json({message : 'internal error, please try again !'});
     }
 });
 
